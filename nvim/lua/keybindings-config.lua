1dd7ec792e59cb1b195bd615104e05b91a50c0de
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2022-11-07 21:16:57 +0800

    feat: keybinding for insert escape

--- nvim/lua/keybindings-config.lua
+++ nvim/lua/keybindings-config.lua
@@ -4,3 +4,6 @@ local map = vim.api.nvim_set_keymap
 map('n', '<leader>e', ':NeoTreeFocusToggle<CR>', {noremap = true, silent = false})
 map('n', '<TAB>', ':BufferLineCycleNext<CR>', {noremap = true, silent = false})
 map('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', {noremap = true, silent = false})
+
+map('i', 'jk', '<ESC>', {noremap = true, silent = false})
+map('i', 'kj', '<ESC>', {noremap = true, silent = false})
