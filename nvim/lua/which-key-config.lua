07fca7aca193d83d4ba2820e476a6c4ad7abd1a6
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2022-11-15 23:34:53 +0800

    feat: lsp config which key bindings

--- nvim/lua/which-key-config.lua
+++ nvim/lua/which-key-config.lua
@@ -7,7 +7,16 @@ local mappings = {
   f = {":Telescope find_files<cr>", "Find Files"},
   r = {":Telescope live_grep<cr>", "Live grep"},
   tr = {":Telescope resume<cr>", "Resume Telescope"},
-  gR = {"<cmd>lua vim.lsp.buf.references()<cr>", "References"}
+
+  -- lsp
+  gr = {"<cmd>lua vim.lsp.buf.references()<cr>", "References"},
+  gD = {"<cmd>lua vim.lsp.buf.declaration()<cr>", "Go to declaration"},
+  gd = {"<cmd>lua vim.lsp.buf.definition()<cr>", "Go to definition"},
+  gi = {"<cmd>lua vim.lsp.buf.implementation()<cr>", "Go to implementation"},
+  K = {"<cmd>lua vim.lsp.buf.hover()<cr>", "View Type"},
+  td = {"<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type definition"},
+  rn = {"<cmd>lua vim.lsp.buf.rename()<cr>", "Rename"},
+  fc = {"<cmd>lua function() vim.lsp.buf.format { async = true } end<cr>", "Format code"},
 }
 
 local opts = {prefix =  '<leader>'}
