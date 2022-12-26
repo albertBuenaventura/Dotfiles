c4c485918af50d04c6c29a4a77c98248838b95c4
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2022-12-26 22:58:45 +0800

    chore: lsp using telescope

--- nvim/lua/which-key-config.lua
+++ nvim/lua/which-key-config.lua
@@ -4,6 +4,8 @@ local mappings = {
   Q = {":wq<cr>", "Save and Quit"},
   w = {":w<cr>", "Save"},
   x = {":bdelete<cr>", "Close current buffer"},
+
+  -- Telescope
   ff = {":Telescope find_files<cr>", "Find Files"},
   fg = {":Telescope live_grep<cr>", "Live grep"},
   fb = {":Telescope buffers<cr>", "Buffers"},
@@ -11,14 +13,13 @@ local mappings = {
   gs = {":Telescope git_status<cr>", "Git Status"},
 
   -- lsp
-  gr = {"<cmd>lua vim.lsp.buf.references()<cr>", "References"},
+  gr = {":Telescope lsp_references<cr>", "References"},
   gD = {"<cmd>lua vim.lsp.buf.declaration()<cr>", "Go to declaration"},
-  gd = {"<cmd>lua vim.lsp.buf.definition()<cr>", "Go to definition"},
-  gi = {"<cmd>lua vim.lsp.buf.implementation()<cr>", "Go to implementation"},
+  gd = {":Telescope lsp_definitions<cr>", "Go to definition"},
+  gi = {"<cmd>lua vim.lsp.buf.implementa<cmd>lua require('telescope.builtin').lsp_definitions()<cr>tion()<cr>", "Go to implementation"},
   K = {"<cmd>lua vim.lsp.buf.hover()<cr>", "View Type"},
   td = {"<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type definition"},
   rn = {"<cmd>lua vim.lsp.buf.rename()<cr>", "Rename"},
-  fc = {"<cmd>lua function() vim.lsp.buf.format { async = true } end<cr>", "Format code"},
 }
 
 local opts = {prefix =  '<leader>'}
