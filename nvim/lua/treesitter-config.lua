d3ab96593e6356df1737651d7250ef009afecb37
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2022-12-27 22:12:57 +0800

    fix: auto indent is not working with nvim-treesitter byt default

--- nvim/lua/treesitter-config.lua
+++ nvim/lua/treesitter-config.lua
@@ -8,6 +8,10 @@ require'nvim-treesitter.configs'.setup {
   -- Automatically install missing parsers when entering buffer
   auto_install = true,
 
+  indent = {
+    enable = true
+  },
+
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
